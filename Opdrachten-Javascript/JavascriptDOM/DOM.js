e7bca8bf49d5368518cf30e362e7c75b12a7450d
geranobeks <gerano007@live.nl>
2018-09-03 09:08:25 +0200

    poging portofolio

---
+++ Opdrachten-Javascript/JavascriptDOM/DOM.js
@@ -0,0 +1,9 @@
+var mijnauto = {
+    merk:"Ford",
+    type:"Mondeo",
+    aantalwielen:"4",
+    kleur:"Blauw"
+}
+
+console.log(mijnauto.kleur);
+document.getElementById("grotekop").innerHTML=mijnauto.kleur
---
+++ wiskunde/dinges.js
@@ -0,0 +1,2 @@
+var randomgetal1 = (Math.floor(Math.random(20)));
+var randomgetal2 = (Math.floor(Math.random(20)));
